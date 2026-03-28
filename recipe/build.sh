ebe01986c15ae67b0db7374c6cd4270080fe321d
Daniel Nachun <daniel.nachun@gmail.com>
2026-03-28 12:08:00 -0700

    try to fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,8 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export CFLAGS="${CFLAGS} -Wno-implicit-function-declaration"
 
-sed -i 's/disable-static //' configure.ac
+export ACLOCAL_PATH=${PREFIX}/share/aclocal
+export ACLOCAL_FLAGS="-I ${PREFIX}/share/aclocal"
 
 ./autogen.sh
 ./configure --prefix=$PREFIX --enable-local-backends --without-gphoto2 --with-usb=yes
